44af0ff98fbdad931bcf38adc295501d251fde17
danielavescia <danivescia@gmail.com>
2026-06-11 15:06:02 -0300

    feat: create User model class with fields for email, name, password, account number, and balance

---
+++ src/main/java/com/bugbank/bugbank_selenium_tests/model/User.java
@@ -0,0 +1,22 @@
+package com.bugbank.bugbank_selenium_tests.model;
+
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private String email;
+
+    private String name;
+
+    private String password;
+
+    private String accountNumber;
+
+    private BigDecimal balance;
+}
