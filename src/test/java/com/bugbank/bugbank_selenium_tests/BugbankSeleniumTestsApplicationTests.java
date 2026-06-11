7a2d65e22d6dd7abe8436650ad9a290c64ba5dc8
danielavescia <danivescia@gmail.com>
2026-06-11 13:53:14 -0300

    chore: initial project setup

---
+++ src/test/java/com/bugbank/bugbank_selenium_tests/BugbankSeleniumTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugbank.bugbank_selenium_tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugbankSeleniumTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
