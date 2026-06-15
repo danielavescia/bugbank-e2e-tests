bf0cf54891aa2dcca8ddbffef6855740920e6111
danielavescia <danivescia@gmail.com>
2026-06-15 09:17:05 -0300

    feat: add BasePage class for Selenium page object model

---
+++ src/main/java/com/bugbank/bugbank_selenium_tests/pages/BasePage.java
@@ -0,0 +1,48 @@
+package com.bugbank.bugbank_selenium_tests.pages;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+
+    protected WebDriver driver;
+
+    protected WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    protected WebElement waitVisible(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected WebElement waitClickable(By locator) {
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    protected void click(By locator){
+         try {
+            waitClickable(locator).click();
+        } catch (Exception e) {
+            WebElement el = waitVisible(locator);
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", el);
+        }
+    }
+
+    protected void type(By locator, String text){
+        WebElement element = waitVisible(locator);
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    public void load(String endPoint){
+        driver.get("https://bugbank.netlify.app" + endPoint);
+    }
+}
