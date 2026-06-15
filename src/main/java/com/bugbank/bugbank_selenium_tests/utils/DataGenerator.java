af47128d33fd26d967ddb1663863fd3b657c7a17
danielavescia <danivescia@gmail.com>
2026-06-15 09:18:25 -0300

    feat: add RegisterComponent and DataGenerator for user registration flow

---
+++ src/main/java/com/bugbank/bugbank_selenium_tests/utils/DataGenerator.java
@@ -0,0 +1,21 @@
+package com.bugbank.bugbank_selenium_tests.utils;
+
+import java.util.Locale;
+import net.datafaker.Faker;
+
+public class DataGenerator {
+    
+    private static final Faker faker = new Faker (new Locale("pt", "BR"));
+
+    public static String generateEmail(){
+        return faker.internet().emailAddress();
+    }
+
+    public static String genereteName(){
+        return faker.name().fullName();
+    }
+    
+    public static String generetePassword(){
+        return faker.internet().password(6, 9);
+    }
+}
