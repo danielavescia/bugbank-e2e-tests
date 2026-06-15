07e0220c92227b07182150feddb7c039102d6567
danielavescia <danivescia@gmail.com>
2026-06-15 09:18:10 -0300

    feat: implement HomePage class for Selenium page object model

---
+++ src/main/java/com/bugbank/bugbank_selenium_tests/pages/HomePage.java
@@ -0,0 +1,36 @@
+package com.bugbank.bugbank_selenium_tests.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import com.bugbank.bugbank_selenium_tests.model.User;
+
+public class HomePage extends BasePage {
+
+    private final By emailField = By.name("email");
+
+    private final By passwordField = By.name("password");
+
+    private final By loginButton = By.xpath("//button[normalize-space()='Acessar']");
+
+    private final By registerButton = By.xpath("(//button[normalize-space()='Registrar'])[1]");
+
+    public HomePage(WebDriver driver){
+        super(driver);
+    }
+
+    public HomePage load(){
+        load("/");
+        return this;
+    }
+
+    public RegisterComponent navigateToRegister() {
+        click(registerButton);
+        return new RegisterComponent(driver);
+    }
+
+    public void Login(User user){
+        type(emailField, user.getEmail());
+        type(passwordField, user.getPassword());
+        click(loginButton);
+    }
+}
