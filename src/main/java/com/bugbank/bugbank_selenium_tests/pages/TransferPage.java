7a230edc94dcc7230f9c7a1b9ab82d4b31aa1f49
danielavescia <danivescia@gmail.com>
2026-06-24 10:15:03 -0300

    feat: Add TransferPage class to handle transfer functionalities in the application

---
+++ src/main/java/com/bugbank/bugbank_selenium_tests/pages/TransferPage.java
@@ -0,0 +1,11 @@
+package com.bugbank.bugbank_selenium_tests.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class TransferPage extends BasePage{
+
+    public TransferPage(WebDriver driver) {
+        super(driver);
+    }
+
+}
