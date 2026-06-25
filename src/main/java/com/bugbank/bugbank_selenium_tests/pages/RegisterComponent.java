cef1ddd48d30b4e68153f796539a6b6016537eca
danielavescia <danivescia@gmail.com>
2026-06-25 10:03:33 -0300

    refactor: Clean up imports and enhance user details assertion in BugbankTest

--- src/main/java/com/bugbank/bugbank_selenium_tests/pages/RegisterComponent.java
+++ src/main/java/com/bugbank/bugbank_selenium_tests/pages/RegisterComponent.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import com.bugbank.bugbank_selenium_tests.model.User;
-
 import io.qameta.allure.Step;
 public class RegisterComponent extends BasePage {
 
